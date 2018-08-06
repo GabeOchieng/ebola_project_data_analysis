bbe2d0232849db9cac6b58d40006e2aa74363cdb
sagekg <dsagekg@gmail.com>
2018-08-06 11:25:18 -0700

    Finalized all code

--- histogram.py
+++ histogram.py
@@ -7,18 +7,20 @@ from tqdm import tqdm
 
 data_mtx = np.load('time_data.npy')
 node_cons = np.load('node_data.npy')
+open('logs/bad_path_log.txt', 'w')
+logfile = open('logs/bad_path_log.txt', 'a')
 print(node_cons)
 legends = []
 for i, row in enumerate(data_mtx):
     for j, data in enumerate(row):
         if j in node_cons[i][1]:
             plt.hist(data)
-            # plt.title("Path from " + str(i) + ' to ' + str(j))
+            plt.title("Path from " + str(i) + ' to ' + str(j))
             plt.xlabel("Seconds")
             plt.ylabel("Number of runs")
             legends.append(str(i) + ' ' + str(j))
             plt.legend(legends, loc=5)
-            # plt.show()
+            plt.show()
         elif len(data):
-            print("Invalid path " + str(i) + ' ' + str(j) + ' ' + str(len(data)))
-plt.show()
+            logfile.write("Invalid path from " + str(i) + ' to ' + str(j) + ', count: ' + str(len(data)) + '\n')
+# plt.show()
